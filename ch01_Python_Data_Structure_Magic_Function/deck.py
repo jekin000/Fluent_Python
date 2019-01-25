2837df856aef0e2aa003f96c96808012501edf6b
jekin000 <kira_r163@163.com>
2019-01-25 15:04:35 +0800

    __getitem__,__len__,__repr__,__add__,__mul__,__bool__

--- ch01_Python_Data_Structure_Magic_Function/deck.py
+++ ch01_Python_Data_Structure_Magic_Function/deck.py
@@ -1,3 +1,7 @@
+##########################################
+# 1.1 The deck by __getitem__, __len__
+##########################################
+
 import collections
 
 Card = collections.namedtuple('Card',['rank','suit'])
@@ -15,6 +19,45 @@ class FrenchDeck:
 	def __getitem__(self,position):
 		return self._cards[position]
 
+
+suit_values = dict(spades=3,hearts=2,diamonds=1,clubs=0)	
+def spades_high(card):
+	rank_value = FrenchDeck.ranks.index(card.rank)
+	return rank_value * len(suit_values) +suit_values[card.suit]
+
 if __name__ == '__main__':
 	beer_card = Card('7','diamonds')
 	print(beer_card)
+
+	#len
+	deck = FrenchDeck()
+	print(len(deck))
+
+	#access by position
+	print(deck[0])
+	print(deck[-1])
+
+	#directly use random.choice
+	from random import choice
+	print (choice(deck))
+	print (choice(deck))
+
+	#in summary
+	#1. you need not remember standard function. Instead, you just use such as len(),sorted() etc.
+	#2. you could easily use Python standard lib
+
+	#slice, __getitem__ use [] on self._cards
+	print(deck[:2])
+	print(deck[12::13])
+
+	#by use __getitem__ we could iterator the deck or reverse(deck)
+	for card in deck: #doctest: +ELLIPSIS
+		print(card)
+
+	#if we do not implement __contains__  'in' will search by order
+	print('-->The ordered "in" ')
+	print(Card('Q','hearts') in deck)
+	print(Card('7','beasts') in deck)
+
+	for card in sorted(deck,key=spades_high):
+		print(card)
